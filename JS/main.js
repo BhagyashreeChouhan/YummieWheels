2782a8633a06e5f8e52b090908b415f26a34f032
BhagyashreeChouhan <chouhanbhagyashree123@gmail.com>
2020-06-12 09:16:29 -0700

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+console.log('js');
+document.getElementById('footer1').addEventListener('click',loadUser);
+function loadUser(){
+var xhr1 = new XMLHttpRequest();
+var url1 = 'https://developers.zomato.com/api/v2.1/dailymenu?res_id=16507624';
+xhr1.open('GET',url1,true);
+// Authentication happens when the portal is open for communication
+xhr1.setRequestHeader('user-key','b5c0c0e737e7b6a6cff87c1279ace7cb');
+xhr1.onload = function()
+{
+    if(this.status == 200){
+       var user1 = this.responseText;      
+       console.log(user1);
+     // var options = JSON.parse(this.responseText);
+     //document.getElementById('search-location').innerHTML = JSON.stringify(options);
+       
+    }
+}
+xhr1.send();
+}
+document.getElementById('footer2').addEventListener('click',loadUser);
+function loadUser(){
+var xhr2 = new XMLHttpRequest();
+var url2 = 'https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318';
+xhr2.open('GET',url2,true);
+// Authentication happens when the portal is open for communication
+xhr2.setRequestHeader('user-key','b5c0c0e737e7b6a6cff87c1279ace7cb');
+xhr2.onload = function()
+{
+    if(this.status == 200){
+       var user2 = this.responseText; 
+       console.log(user2);
+     // var options = JSON.parse(this.responseText);
+     //document.getElementById('search-location').innerHTML = JSON.stringify(options);
+       
+    }
+}
+xhr2.send();
+}
